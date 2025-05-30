78800cb52d0277e25ac40e16b0c7efb26ce58374
Enrico Proment <giuseppe.proment@gmail.com>
2025-05-30 17:18:29 -0300

    house keeping

--- functional_through_lambda_calculus/start.hs
+++ functional_through_lambda_calculus/start.hs
@@ -5,6 +5,7 @@
 {-# HLINT ignore "Use const" #-}
 {-# HLINT ignore "Use id" #-}
 {-# HLINT ignore "Redundant bracket" #-}
+{-# HLINT ignore "Avoid lambda" #-}
 
 selectFirst :: first -> second -> first
 selectFirst = \first -> \second -> first
@@ -14,4 +15,19 @@ selectSecond = \first -> \second -> second
 
 make_pair :: first -> second -> (first -> second -> func) -> func
 make_pair = \first -> \second -> \func -> ((func first) second)
+
+-- boolean logic
+
+cond :: e1 -> e2 -> (e1 -> e2 -> c) -> c
+cond = \e1 -> \e2 -> \c -> ((c e1) e2)
+
+true = selectFirst
+false = selectSecond
+not_ = \x -> ((x false) true)
+
+toBool = \b -> (b True False)
+
+-- sum logic
+_2_ = \f -> \x -> f( f x )
+_3_ = \f -> \x -> f( f( f x ))
 
